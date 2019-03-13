ae41f6a4c8772d9b3eb6977f48b21e15da5b5800
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2019-03-13 15:49:23 +0000

    Started work on bar growth

---
+++ SkillsGroupGame/Assets/_Scripts/BarExtend.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BarExtend : MonoBehaviour
+{
+    Transform t;
+    Vector3 grow = new Vector3(0.5f, 0.0f, 0.0f);
+
+    private void Start()
+    {
+        t = this.transform;
+    }
+
+    public void PointerEnter()
+    {
+        Debug.Log("Triggered!");
+        Grow();
+    }
+
+    public void PointerExit()
+    {
+        Shrink();
+    }
+
+    public void Grow()
+    {
+        Debug.Log("Grow!");
+
+        while (t.localScale.x < 25.0f)
+        {
+            Debug.Log("lol");
+            t.localScale += grow;
+            new WaitForSeconds(0.5f);
+        }
+        
+    }
+
+    public void Shrink()
+    {
+        Debug.Log("Shrink!");
+
+        while (t.localScale.x > 1f)
+        {
+            t.localScale -= grow;
+            new WaitForSeconds(0.5f);
+        }
+
+        
+    }
+
+  
+}
---
+++ SkillsGroupGame/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayAutoGame()
+    {
+        Debug.Log("Playing Automotive game!");
+    }
+
+    public void PlayEngineerGame()
+    {
+        Debug.Log("Playing Engineering game!");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Opening settings!");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting game!");
+        Application.Quit();
+    }
+
+    private void OnMouseEnter()
+    {
+    }
+
+}
