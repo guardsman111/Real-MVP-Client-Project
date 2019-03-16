67b6beeee01fac230f3eeda1a6e00cbed2ee7631
Guardsman111 [home] <charliep763@googlemail.com>
2019-03-16 18:12:22 +0000

    0.02
    Added Click-Dragging to parts

--- SkillsGroupGame/Assets/Scripts/Part_Script.cs
+++ SkillsGroupGame/Assets/Scripts/Part_Script.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Part_Script : MonoBehaviour
 {
+    private Camera camera;
+    public bool isBroken = false;
+
     // Start is called before the first frame update
+    // Randomly declares the part broken or leaves it as its default, inspector set value.
     void Start()
     {
-        
+        int r = Random.Range(0,2);
+
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        if (r >= 1)
+            isBroken = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Debug.Log("Mouse Down" + gameObject.name);
     }
+
+    private void OnMouseDrag()
+    {
+        Vector3 offset = new Vector3(0.0f, 0.0f, -10.0f);
+        transform.position = camera.ScreenToWorldPoint(Input.mousePosition) - offset;
+    }
+
 }
