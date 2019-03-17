1a7bff68d0cd8346ae7a485772b1ee894414ea6d
Guardsman111 [home] <charliep763@googlemail.com>
2019-03-17 23:24:26 +0000

    Mechanic Finished
    Can delete old parts and add new parts
    
    Parts are randomly broken on start.
    
    Old Parts are dropped into the bin to be deleted
    
    No win Scenario, or check button yet

---
+++ SkillsGroupGame/Assets/Scripts/New_Part_Slot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_Part_Slot : MonoBehaviour
+{
+    public GameObject newPartPrefab;
+    public Transform spawn;
+
+    // Creates a new copy of the part type under the spawn point
+    private void OnMouseDown()
+    {
+        GameObject newPart = Instantiate(newPartPrefab, spawn.position, transform.rotation);
+        newPart.GetComponent<Part_Script>().emptyPos = spawn;
+    }
+}
--- SkillsGroupGame/Assets/Scripts/Part_Script.cs
+++ SkillsGroupGame/Assets/Scripts/Part_Script.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Part_Script : MonoBehaviour
 {
-    private Camera camera;
+    private Camera sceneCamera;
+    public Sprite brokenSprite;
+    public Transform emptyPos;
     public bool isBroken = false;
 
-    // Start is called before the first frame update
+    private bool overBin = false;
+    public bool canBreak = false;
+
     // Randomly declares the part broken or leaves it as its default, inspector set value.
     void Start()
     {
-        int r = Random.Range(0,2);
+        sceneCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 
-        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        if (canBreak)
+            CheckBroken();
+    }
+
+    public void CheckBroken()
+    {
+        int r = Random.Range(0, 2);
 
         if (r >= 1)
             isBroken = true;
+
+        if (isBroken)
+            GetComponent<SpriteRenderer>().sprite = brokenSprite;
     }
 
     private void OnMouseDown()
@@ -24,10 +37,49 @@ public class Part_Script : MonoBehaviour
         Debug.Log("Mouse Down" + gameObject.name);
     }
 
+    // Moves the part with the mouse
     private void OnMouseDrag()
     {
         Vector3 offset = new Vector3(0.0f, 0.0f, -10.0f);
-        transform.position = camera.ScreenToWorldPoint(Input.mousePosition) - offset;
+        transform.position = sceneCamera.ScreenToWorldPoint(Input.mousePosition) - offset;
     }
 
+    // Drops the part
+    private void OnMouseUp()
+    {
+        if (overBin)
+        {
+            Destroy(gameObject, 0.0f);
+        }
+        else
+        {
+            transform.position = emptyPos.position;
+            emptyPos.gameObject.GetComponent<Empty_Part_Script>().setFull(true);
+        }
+    }
+
+    // Declares what the part is being held over
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Bin")
+        {
+            overBin = true;
+            emptyPos.gameObject.GetComponent<Empty_Part_Script>().setFull(false);
+        }
+        else if (collision.gameObject.tag == gameObject.tag)
+        {
+            // If part slot is empty
+            if (!collision.gameObject.GetComponent<Empty_Part_Script>().isFull)
+            emptyPos = collision.gameObject.transform;
+        }
+        Debug.Log(gameObject.name + "Collision");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bin")
+        {
+            overBin = false;
+        }
+    }
 }
