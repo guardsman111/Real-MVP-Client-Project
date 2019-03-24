203975eba4f0761d66085ffddf1397349232964f
Guardsman111 [home] <charliep763@googlemail.com>
2019-03-24 17:00:59 +0000

    Completion
    Can now complete repair, gives numbe of parts still broken and time taken

---
+++ SkillsGroupGame/Assets/Scripts/Completion_Script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Completion_Script : MonoBehaviour
+{
+    public GameObject parent;
+
+    public void WriteResult(int brokenPartsMissed, float TimeTaken)
+    {
+        this.GetComponent<Text>().text = ("Number of broken parts missed - " + brokenPartsMissed + "\n\nTime taken to effect repairs - " + TimeTaken);
+        parent.SetActive(true);
+    }
+}
---
+++ SkillsGroupGame/Assets/Scripts/Done_Button_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Done_Button_Script : MonoBehaviour
+{
+    public GameObject partsParent;
+    private Part_Script[] partsArray;
+    private int partsBroken = 0;
+
+    public Completion_Script display;
+
+    public void CheckParts()
+    {
+        partsArray = partsParent.GetComponentsInChildren<Part_Script>();
+        partsBroken = 0;
+        foreach (Part_Script part in partsArray)
+        {
+            if (part.isBroken == true)
+            {
+                partsBroken += 1;
+            }
+        }
+        display.WriteResult(partsBroken, Time.timeSinceLevelLoad);
+    }
+
+    private void OnMouseUp()
+    {
+        CheckParts();
+    }
+}
