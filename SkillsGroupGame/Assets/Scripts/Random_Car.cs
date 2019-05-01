955d720848984784461089d1ac00da2e925ca4b0
Guardsman111 [home] <charliep763@googlemail.com>
2019-05-01 17:52:32 +0100

    Art Implementation 1
    Implemented all art from part 1

---
+++ SkillsGroupGame/Assets/Scripts/Random_Car.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_Car : MonoBehaviour
+{
+    public Sprite carBlue;
+    public Sprite carGreen;
+    public Sprite carRed;
+    public Sprite carPurple;
+    public Sprite carYellow;
+
+    private Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprites = new Sprite[] { carBlue, carGreen, carPurple, carRed, carYellow };
+        int number = Random.Range(0, 4);
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[number];
+    }
+
+}
